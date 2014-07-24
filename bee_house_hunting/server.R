ac1f9e15ed401591f12c9b7e29c5cb9cfcdc6f3c
Simon Garnier <simon.garnier@gmail.com>
2014-07-23 22:40:12 -0400

    Updated honeybee nest hunting model to react to changes in the interface. Also completed the interface.

--- bee_house_hunting/server.R
+++ bee_house_hunting/server.R
@@ -1,7 +1,7 @@
 # Required libraries
-require("shiny")
-require("deSolve")
-require("ggplot2")
+library(shiny)
+library(deSolve)
+library(ggplot2)
 
 
 # ODE function
@@ -27,28 +27,27 @@ ode_sys <- function(t, y, parms) {
 }
 
 
-# Parameters
-parms <- list(gamma = c(1, 1, 1),         # discovery rate
-              alpha = c(1/3, 1/3, 1/3),   # spontaneous uncommitment 
-              rho = c(3, 3, 3),           # recruitment rate 
-              sigma = c(10, 10, 10),      # conversion rate
-              theta = 10)                 # time at which 3rd nest is introduced
-
-
 # Shiny server call
 shinyServer(function(input, output) {
   
   output$distPlot <- renderPlot({
-    out <- lsoda(c(0, 0, 0), 0:250 / 10, ode_sys, parms)
+    parms <- list(gamma = c(input$gamma1, input$gamma2, input$gamma3),  # discovery rate
+                  alpha = c(input$alpha1, input$alpha2, input$alpha3),  # spontaneous uncommitment 
+                  rho = c(input$rho1, input$rho2, input$rho3),          # recruitment rate 
+                  sigma = c(input$sigma1, input$sigma2, input$sigma3),  # conversion rate
+                  theta = input$theta)                                  # time at which 3rd nest is introduced
+    
+    out <- lsoda(c(0, 0, 0), seq(0, input$duration, 0.1), ode_sys, parms)
+    out[, 2:4] <- out[, 2:4] * input$scouts
     out <- as.data.frame(out)
     names(out) <- c("time", paste0("nest", 1:3))
     
-    g<- ggplot(data = out,
-               aes(x = time)) + 
-      geom_path(aes(y = nest1 * 100), size = 1, color = "red") + 
-      geom_path(aes(y = nest2 * 100), size = 1, color = "blue") +
-      geom_path(aes(y = nest3 * 100), size = 1, color = "green") +
-      geom_hline(yintercept = 25, size = 1, linetype = 2) +
+    g <- ggplot(data = out,
+                aes(x = time)) + 
+      geom_path(aes(y = nest1), size = 1, color = "red") + 
+      geom_path(aes(y = nest2), size = 1, color = "blue") +
+      geom_path(aes(y = nest3), size = 1, color = "green") +
+      geom_hline(yintercept = input$quorum, size = 1, linetype = 2) +
       theme_minimal(base_size = 14)
     
     print(g)
--- bee_house_hunting/ui.R
+++ bee_house_hunting/ui.R
@@ -1,63 +1,59 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
-
+  
   # Application title
   titlePanel("Collective nest-selection in honeybees"),
-
+  
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       sliderInput("duration", "Duration of the trial:",
-                  min = 1, max = 1000, value = 25, width = "100%"),
+                  min = 1, max = 200, value = 200, width = "100%"),
       sliderInput("scouts", "Number of scouts:",
                   min = 1, max = 200, value = 100, width = "100%"),
       sliderInput("quorum", "Quorum number:",
                   min = 1, max = 200, value = 25, width = "100%"),
-      sliderInput("three", "Time to introduce the 3rd nest:",
+      sliderInput("theta", "Time to introduce the 3rd nest:",
                   min = 1, max = 200, value = 100, width = "100%")
     ),
-
+    
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("distPlot"),
       fluidRow(
         column(4,
-               sliderInput("a", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("b", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("c", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("d", "Number of observations:",
-                           min = 1, max = 1000, value = 500)
+               h5("Nest #1"),
+               sliderInput("gamma1", "Discovery rate:",
+                           min = 0, max = 10, value = 1.5, step = 0.1),
+               sliderInput("alpha1", "Desertion rate:",
+                           min = 0, max = 10, value = 1, step = 0.1),
+               sliderInput("rho1", "Recruitment rate:",
+                           min = 0, max = 10, value = 2, step = 0.1),
+               sliderInput("sigma1", "Conversion rate:",
+                           min = 0, max = 10, value = 2, step = 0.1)
         ),
         column(4,
-               sliderInput("a", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("b", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("c", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("d", "Number of observations:",
-                           min = 1, max = 1000, value = 500)
+               h5("Nest #2"),
+               sliderInput("gamma2", "Discovery rate:",
+                           min = 0, max = 10, value = 2, step = 0.1),
+               sliderInput("alpha2", "Desertion rate:",
+                           min = 0, max = 10, value = 1.5, step = 0.1),
+               sliderInput("rho2", "Recruitment rate:",
+                           min = 0, max = 10, value = 2, step = 0.1),
+               sliderInput("sigma2", "Conversion rate:",
+                           min = 0, max = 10, value = 2, step = 0.1)
         ),
         column(4,
-               sliderInput("a", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("b", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("c", "Number of observations:",
-                           min = 1, max = 1000, value = 500),
-               sliderInput("d", "Number of observations:",
-                           min = 1, max = 1000, value = 500)
+               h5("Nest #3"),
+               sliderInput("gamma3", "Discovery rate:",
+                           min = 0, max = 10, value = 2, step = 0.1),
+               sliderInput("alpha3", "Desertion rate:",
+                           min = 0, max = 10, value = 1, step = 0.1),
+               sliderInput("rho3", "Recruitment rate:",
+                           min = 0, max = 10, value = 2, step = 0.1),
+               sliderInput("sigma3", "Conversion rate:",
+                           min = 0, max = 10, value = 2, step = 0.1)
         )
       )
     )
