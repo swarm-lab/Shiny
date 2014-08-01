963dd4ee435556738a25fa1e31e11efb48c8446f
Simon Garnier <simon.garnier@gmail.com>
2014-08-01 13:35:52 -0400

    Removed more unnecessary code.

--- opinion_dynamic/grid_sys.R
+++ opinion_dynamic/grid_sys.R
@@ -1,19 +1,3 @@
-# grid_sys <- function(time, init, parms) {
-#   committed <- eightneighbours(init != 0)
-#   total <- eightneighbours(init)
-#   maximum <- eightneighbours(abs(init))
-#   
-#   p1 <- runif(nrow(init) * ncol(init)) <= (committed / 8)
-#   p2 <- runif(nrow(init) * ncol(init)) <= ((total + maximum) / (2 * maximum))
-#   
-#   idx <- committed > 0
-#   
-#   init[(idx & p1 & p2)] <- 1 * parms$w1  
-#   init[(idx & p1 & !p2)] <- -1 * parms$w2  
-#   
-#   init
-# }
-
 grid_sys <- function(time, init, parms) {
   o1 <- init
   o1[o1 < 0] <- 0
